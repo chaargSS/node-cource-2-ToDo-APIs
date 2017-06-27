2880531cee54e5362405ca3b44d68355fd0a7823
chaaragSS <network.codeit@gmail.com>
2017-06-27 22:54:14 +0530

    adding Logging In test route for  /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -249,4 +249,54 @@ describe('POST /users',()=>{
         .end(done)
         
      });
-})
+});
+
+describe('POST /users/login',()=>{
+       
+        it('should login user and return auth token',(done)=>{
+               
+                request(app)
+                .post('/users/login')
+                .send({Email:users[1].Email,password:users[1].password})
+                .expect(200)
+                .expect((res)=>{
+                    expect(res.headers['x-auth']).toExist();
+                    expect(res.body).toNotEqual({});
+                    expect(res.body._id).toExist();
+                })
+                .end((err,res)=>{
+                    if(err){  return done(err)  }
+
+                    User.findById(users[1]._id).then((user)=>{
+                        expect(user.tokens[0]).toInclude({
+                            access:'auth',
+                            token:res.headers['x-auth']
+                        });
+                            done();
+                        }).catch((e)=>done(e));
+                });
+
+        });
+
+        it('should reject invalid user',(done)=>{
+
+                 request(app)
+                .post('/users/login')
+                .send({Email:'abc@gmail',password:'abc123'})
+                .expect(400)
+                .expect((res)=>{
+                     expect(res.body).toEqual({});
+                       expect(res.headers['x-auth']).toNotExist();
+                })
+                .end((err,res)=>{
+                    if(err){  return done(err)  }
+
+                    User.findById(users[1]._id).then((user)=>{
+                        expect(user.tokens.length).toBe(0) 
+                            done();
+                        }).catch((e)=>done(e));
+                     });
+
+                }); 
+
+});
